2aec0a42090ba1d42cc38155dbd399146528748a
Dieuwertjesara <37107193+Dieuwertjesara@users.noreply.github.com>
2018-03-14 17:15:01 +0100

    make background work

--- challenge2.js
+++ challenge2.js
@@ -2,27 +2,27 @@ function getStylesheet() {
   var now = new Date();
   var hours = now.getHours();
   //ochtend 6-10
-      if (hours > 6 && hours < 10) {
+      if (hours >= 6 && hours < 10) {
        document.write("<link rel='stylesheet' href='morning.css' type='text/css'>");
       }
   //overdag 10-12
-      else if (hours > 10 && hours < 12) {
+      else if (hours >= 10 && hours < 12) {
        document.write("<link rel='stylesheet' href='morning.css' type='text/css'>");
       }
   //middag 12-17
-      else if (hours > 12 && hours < 17) {
+      else if (hours >= 12 && hours < 17) {
        document.write("<link rel='stylesheet' href='day.css' type='text/css'>");
       }
   //namiddag 17-20
-      else if (hours > 17 && hours < 20) {
+      else if (hours >= 17 && hours < 20) {
        document.write("<link rel='stylesheet' href='evening.css' type='text/css'>");
       }
   //avond 20-24
-      else if (hours > 20 && hours < 24) {
+      else if (hours >= 20 && hours < 24) {
        document.write("<link rel='stylesheet' href='night.css' type='text/css'>");
       }
   //avond 24-6
-      else if (hours > 0 && hours < 6) {
+      else if (hours >= 24 && hours < 6) {
         document.write("<link rel='stylesheet' href='night.css' type='text/css'>");
           }
       else {
