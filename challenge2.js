2eab3bf4a1ab98798016b671d5a943e82516f64b
Dieuwertjesara <37107193+Dieuwertjesara@users.noreply.github.com>
2018-03-17 00:48:35 +0100

    complete animation in default css

--- challenge2.js
+++ challenge2.js
@@ -1,22 +1,23 @@
 
 
 function getStylesheet() {
-      var currentTime =  new Date().getHours();
-      if (0 <= currentTime&&currentTime < 6) {
+      var currentTime = new Date().getHours();
+      if (1 <= currentTime&&currentTime < 6) {
        document.write("<link rel='stylesheet' href='night.css' type='text/css'>");
       }
-      if (6 <= currentTime&&currentTime < 9) {
+      if (6 <= currentTime&&currentTime < 12) {
        document.write("<link rel='stylesheet' href='morning.css' type='text/css'>");
       }
-      if (9 <= currentTime&&currentTime < 17) {
+      if (12 <= currentTime&&currentTime < 18) {
        document.write("<link rel='stylesheet' href='day.css' type='text/css'>");
       }
-      if (16 <= currentTime&&currentTime < 22) {
+      if (18 <= currentTime&&currentTime < 24) {
        document.write("<link rel='stylesheet' href='evening.css' type='text/css'>");
       }
-      if (22 <= currentTime&&currentTime <= 24) {
+      if (24 <= currentTime&&currentTime <= 24) {
        document.write("<link rel='stylesheet' href='night.css' type='text/css'>");
       }
+
 }
 
 getStylesheet();
@@ -58,7 +59,7 @@ function startTime() {
     s = checkTime(s);
     document.getElementById('txt').innerHTML =
     h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
+    var t = setTimeout(startTime, 1000);
 }
 function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
